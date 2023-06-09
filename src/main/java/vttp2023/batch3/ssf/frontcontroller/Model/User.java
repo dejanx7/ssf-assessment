1bb275289c00e228e89b15d6ebc89d93ccac7478
dejanx7 <arunnarenderan1@gmail.com>
2023-06-09 12:31:58 +0800

    update

---
+++ src/main/java/vttp2023/batch3/ssf/frontcontroller/Model/User.java
@@ -0,0 +1,49 @@
+package vttp2023.batch3.ssf.frontcontroller.Model;
+
+import java.io.Serializable;
+
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class User implements Serializable {
+
+    @Size(min= 2, message = "Please enter a valid username.")
+    private String username;
+    @Size(min= 2, message = "Please enter a valid password.")
+    private String password;
+    
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+   
+
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public JsonObject toJson(){
+
+        return Json.createObjectBuilder()
+        .add("username", this.getUsername())
+        .add("password", this.getPassword())
+        .build();
+    }
+
+    
+    
+}
--- src/main/java/vttp2023/batch3/ssf/frontcontroller/controllers/FrontController.java
+++ src/main/java/vttp2023/batch3/ssf/frontcontroller/controllers/FrontController.java
@@ -1,7 +1,52 @@
 package vttp2023.batch3.ssf.frontcontroller.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.validation.Valid;
+import vttp2023.batch3.ssf.frontcontroller.Model.User;
+
+
+@Controller
+@RequestMapping
 public class FrontController {
 
 	// TODO: Task 2, Task 3, Task 4, Task 6
-	
+	@GetMapping(path = "/")
+	public String showLoginPage(User user, Model m){
+
+		m.addAttribute("user", user);
+
+		return "view0";
+	}
+
+
+	@PostMapping(path="/login", consumes = "application/x-www-form-urlencoded", produces = "text/html")
+	public String loginUser(@Valid User user, BindingResult br, Model m ){
+
+		if (br.hasErrors()){
+
+			return "view0";
+			
+		}else {
+
+			
+			return "test";
+
+		}
+
+		
+
+		
+	}
+
+
+
+
 }
